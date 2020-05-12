0228d75a43e234d5049ae134f9ea1ac24eb61ac6
Pradhvan <pradhvanbisht@gmail.com>
2020-05-12 11:46:59 +0530

    BugFix: Do not show caption on missing caption key

--- tests/test_tranform.py
+++ tests/test_tranform.py
@@ -1,6 +1,7 @@
 import os
 import pytest
 import tiptapy
+import json
 from tiptapy import extras
 
 
@@ -35,8 +36,39 @@ def build_test_data():
 json_data, html_data = build_test_data()
 
 
+def build_failing_caption_data(mediatag):
+    """
+    Manipulate mediatags input json data to return a dict with
+    empty caption and no caption key.
+    """
+    transfomed_data = {}
+    input_data = json.loads(json_data[mediatag])
+    del input_data['attrs']['caption']
+    transfomed_data['no_caption_key'] = input_data
+    input_data['attrs']['caption'] = ''
+    transfomed_data['empty_caption'] = input_data
+    return transfomed_data
+
+
 @pytest.mark.parametrize("tag", tags_to_test)
 def test_html_tag(tag):
+    """
+    Test expected json input with the expected html.
+    """
     tag_data = json_data[tag]
     expected_html = html_data[tag]
     assert tiptapy.to_html(tag_data) == expected_html
+
+
+@pytest.mark.parametrize("mediatag",
+                         ["image", "featuredimage", "embed"]
+                         )
+def test_mediatag_caption(mediatag):
+    """
+    Test missing caption key in json input with the expected html.
+    """
+    transfomed_data = build_failing_caption_data(mediatag)
+    for data in transfomed_data:
+        tag_data = transfomed_data[data]
+        expected_html = html_data[mediatag]
+        assert tiptapy.to_html(tag_data) != expected_html
